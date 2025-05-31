b3f1650018a55ad7be3807de62ccc98a36fb1fbe
marcoscondejr <marcos14conde@hotmail.com>
2025-05-31 15:44:04 -0300

    feat: criando a conexão com o banco

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const connectDB = require('./config/database');
+const express = require('express');
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.status(200).json({Sucess: "Olá Mundo"});
+})
+
+app.listen(3000, () => {
+    console.log('Servidor Rodando em http://127.0.0.1:3000');
+})
